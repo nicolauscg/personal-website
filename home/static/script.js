548eabeece8aade6815cf160034897efb635ad39
Nicolaus C <nicolauscg@gmail.com>
2019-02-21 22:34:48 +0700

    tidy scripts

--- home/static/script.js
+++ home/static/script.js
@@ -4,73 +4,100 @@ $(document).ready(function() {
     $('.navbar').toggleClass('open');
   });
   
-  // move text based on cursor position
-  var elementToBeMoved = $('.hero__large-text');
-  if (elementToBeMoved.length) {
-    var windowWidth = $(window).width();
-    var windowHeight = $(window).height();
-    var factor = 100;
-    $(window).mousemove(function(event) {
-      elementToBeMoved.css('top', (windowHeight/2 - event.pageY)/factor );
-      elementToBeMoved.css('left', (windowWidth/2 - event.pageX)/factor );
+  movingText($('.hero__large-text'));
+
+  scrambleNumber($('.scramble-number'));
+
+  typewriter($('.typewriter'));
+
+  
+  function movingText(selector, attributes) {
+    selector.each(function(element) {
+      var window =  $(window);
+      var windowWidth = window.width();
+      var windowHeight = window.height();
+      var factor = attributes["factor"] ? attributes["factor"] : 100;
+      // adjust top and left values depending on mouse position
+      window.mousemove(function(event) {
+        element.css('top', (windowHeight/2 - event.pageY)/factor );
+        element.css('left', (windowWidth/2 - event.pageX)/factor );
+      });
     });
   }
 
-  var numberElements = $('.scramble-number');
-
-  function intervalFunction() {
-    return Math.floor((Math.random() * 80) + 1);
-  }
-
-  function scrambleNumber(element) {
-    var initial = element.innerHTML.split('');
-
-    for(var i=0; i<initial.length; i++) {
-      scramble(element, i, initial[i], 15, intervalFunction);
-    }
-  }
-  function scramble(element, digit, initial, count, interval) {
-    var newNumber = element.innerHTML.split('');
-    if (count <= 0) {
-      newNumber[digit] = initial;
-      element.innerHTML = newNumber.join('');
-      return;
-    }
-    newNumber[digit] = Math.floor((Math.random() * 9) + 1);
-    element.innerHTML = newNumber.join('');
-    setTimeout(scramble, interval(), element, digit, initial, count - 1, interval)
-  }
-  numberElements.each(function() {
-    scrambleNumber(this)
-  });
-
-  // typewriter effect
-  var elementSelector = $('.typewriter');
-  if (elementSelector.length) {
-    var linesOfText = elementSelector.html().split("<br>");
-    var totalCharCount = linesOfText.join('').length;
-    var initialHeight = elementSelector.css('height');
-    elementSelector.html('');
-    elementSelector.css('height', initialHeight);
-    var i = 0;
-    var charIndex = 0;
-    var lineIndex = 0;
-    var speed = 50;
-    function typeWriter() {
-      if (i < totalCharCount){
-        if (charIndex == linesOfText[lineIndex].length) {
-          charIndex = 0;
-          lineIndex++;
-          elementSelector.html(elementSelector.html() + '<br>');
+  function scrambleNumber(selector, attributes={}) {
+    var intervalMin = attributes["intervalMin"] ? attributes["intervalMin"] : 20;
+    var intervalMax = attributes["intervalMax"] ? attributes["intervalMax"] : 80;
+    var intervalFunc = getIntervalFunction(intervalMin, intervalMax);
+    var scrambleCount = attributes["count"] ? attributes["count"] : 15;
+    // for every element in selector
+    selector.each(function() {
+      var element = this;
+      var initialNumber = element.innerHTML.split('');
+      // for every digit in element
+      $.each(initialNumber, function(index, obj) {
+        // scramble index-th digit scrambleCount times, 
+        // then reapply original number in index-th digit
+        scrambleDigit(scrambleCount);
+        function scrambleDigit(count) {
+          var temp = element.innerHTML.split('');
+          if (count <= 0) {
+            temp[index] = initialNumber[index];
+            element.innerHTML = temp.join('');
+            return;
+          }
+          temp[index] = Math.floor((Math.random() * 9) + 1);
+          element.innerHTML = temp.join('');
+          setTimeout(scrambleDigit, intervalFunc(), --count);
         }
-        elementSelector.html(elementSelector.html() + linesOfText[lineIndex].charAt(charIndex));
-        i++;
-        charIndex++;
-        setTimeout(typeWriter, speed);
-      } else {
-        elementSelector.css('height', 'fit-content');
+      }); 
+    });
+    
+    // returns a function that will be called to get randomized number between min max
+    // used in setTimout to delay scrambling digits
+    function getIntervalFunction(min, max) {
+      return function() {
+        return Math.floor((Math.random() * (max-min)) + min);
       }
     }
-    typeWriter();
+  }
+
+  function typewriter(selector, attributes={}) {
+    var speed = attributes["interval"] ? attributes["interval"] : 40;
+
+    $.each(selector, function() {
+      var element = this;
+      var initialHeight = element.offsetHeight;
+      var linesOfText = element.innerHTML.trim().split("<br>");
+      var totalCharCount = linesOfText.join('').length;
+      // empty content but make div height same as before
+      element.innerHTML = '';
+      element.style.height = initialHeight+"px";
+
+      var i = 0;
+      var charIndex = 0; // index of character in current line
+      var lineIndex = 0; // index of line
+
+      typeWriter();
+
+      function typeWriter() {
+        if (i < totalCharCount){
+          // if end of line, add newline char
+          if (charIndex == linesOfText[lineIndex].length) {
+            charIndex = 0;
+            lineIndex++;
+            element.innerHTML += '<br>';
+          } 
+          element.innerHTML += linesOfText[lineIndex].charAt(charIndex);
+          i++;
+          charIndex++;
+          
+          setTimeout(typeWriter, interval);
+        } else {
+          element.style.height = 'fit-content';
+        }
+      }
+    });
+
   }
 });
